5a7c7a0147e60a9a6138300c02b4065feab9555c
Al-ImranRony <imranrony687@gmail.com>
2020-09-21 17:49:38 +0600

    UI design

--- Merchandise_Solution/Vendor_Automation/Vendor_Automation/settings.py
+++ Merchandise_Solution/Vendor_Automation/Vendor_Automation/settings.py
@@ -11,6 +11,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
