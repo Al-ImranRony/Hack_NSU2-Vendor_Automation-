64b1ca706fd0842330a5496d19d510ee8d1134da
Al-ImranRony <imranrony687@gmail.com>
2020-09-21 10:45:57 +0600

    Base & profile

--- Merchandise_Solution/Vendor_Automation/users/forms.py
+++ Merchandise_Solution/Vendor_Automation/users/forms.py
@@ -1,13 +1,34 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     company_name = forms.CharField(max_length=100)
-    cell = forms.CharField(max_length=100)
+    cell = forms.IntegerField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'company_name', 'cell', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    company_name = forms.CharField(max_length=100)
+    cell = forms.IntegerField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'company_name', 'cell']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    company_name = forms.CharField(max_length=100)
+    cell = forms.IntegerField()
+
+    class Meta:
+        model = Profile
+        fields = ['email', 'company_name', 'cell']
--- Merchandise_Solution/Vendor_Automation/users/models.py
+++ Merchandise_Solution/Vendor_Automation/users/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    email = models.EmailField(max_length=70, null=True, blank=True, unique=True)
+    company_name = models.CharField(max_length=100, default='ABC')
+    cell = models.IntegerField(unique=True, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.user.username} {self.user.email} {self.user.company_name} {self.user.cell} Profile'
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
